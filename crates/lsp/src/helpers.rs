14f166b651cc8549a85409682cbb4a209425d3f7
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-04-22 17:46:18 +0800

    r-a span to lsp span

--- crates/lsp/src/helpers.rs
+++ crates/lsp/src/helpers.rs
@@ -1,10 +1,89 @@
-// FIXME: removed pest: use pest::{error::LineColLocation, Span};
-use tower_lsp::lsp_types::{Location, Position, Range, Url};
+use std::iter;
 
-pub(crate) trait IntoRange {
-    fn into_lsp_range(self) -> Range;
+// FIXME: removed pest: use pest::{error::LineColLocation, Span};
+use syntax::parse::TextRange;
+use syntax::parse::TextSize;
+use tower_lsp::lsp_types::{Position, Range};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct LineIndex {
+    /// Offset the beginning of each line, zero-based.
+    pub(crate) newlines: Vec<TextSize>,
 }
 
-pub trait IntoLocation {
-    fn into_lsp_location(self, uri: &Url) -> Location;
+/// Line/Column information in native, utf8 format.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct LineCol {
+    /// Zero-based
+    pub line: u32,
+    /// Zero-based utf8 offset
+    pub col: u32,
+}
+
+impl LineIndex {
+    pub fn new(text: &str) -> LineIndex {
+        let mut newlines = Vec::with_capacity(16);
+        newlines.push(TextSize::from(0));
+
+        let mut curr_row = 0.into();
+        let mut curr_col: TextSize = 0.into();
+        let mut line = 0;
+        for c in text.chars() {
+            let c_len = TextSize::of(c);
+            curr_row += c_len;
+            if c == '\n' {
+                newlines.push(curr_row);
+
+                // Prepare for processing the next line
+                curr_col = 0.into();
+                line += 1;
+                continue;
+            }
+
+            curr_col += c_len;
+        }
+
+        // Save any utf-16 characters seen in the last line
+        LineIndex { newlines }
+    }
+
+    pub fn line_col(&self, offset: TextSize) -> LineCol {
+        let line = self.newlines.partition_point(|&it| it <= offset) - 1;
+        let line_start_offset = self.newlines[line];
+        let col = offset - line_start_offset;
+        LineCol {
+            line: line as u32,
+            col: col.into(),
+        }
+    }
+
+    pub fn offset(&self, line_col: LineCol) -> Option<TextSize> {
+        self.newlines
+            .get(line_col.line as usize)
+            .map(|offset| offset + TextSize::from(line_col.col))
+    }
+
+    pub fn lines(&self, range: TextRange) -> impl Iterator<Item = TextRange> + '_ {
+        let lo = self.newlines.partition_point(|&it| it < range.start());
+        let hi = self.newlines.partition_point(|&it| it <= range.end());
+        let all = iter::once(range.start())
+            .chain(self.newlines[lo..hi].iter().copied())
+            .chain(iter::once(range.end()));
+
+        all.clone()
+            .zip(all.skip(1))
+            .map(|(lo, hi)| TextRange::new(lo, hi))
+            .filter(|it| !it.is_empty())
+    }
+}
+
+pub(crate) fn position(line_index: &LineIndex, offset: TextSize) -> Position {
+    let line_col = line_index.line_col(offset);
+    Position::new(line_col.line, line_col.col)
+}
+
+pub(crate) fn range(line_index: &LineIndex, range: TextRange) -> Range {
+    let start = position(line_index, range.start());
+    let end = position(line_index, range.end());
+    Range::new(start, end)
 }
