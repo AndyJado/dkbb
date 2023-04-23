b74be2a835dba2ee4b6956308768a5b2ab0dfdbd
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-04-23 16:00:04 +0800

    line index across lsp and ra

--- crates/lsp/src/helper/line_index.rs
+++ crates/lsp/src/helper/line_index.rs
@@ -1,3 +1,5 @@
+use anyhow::format_err;
+use anyhow::Result;
 use std::iter;
 
 // FIXME: removed pest: use pest::{error::LineColLocation, Span};
@@ -8,7 +10,7 @@ use tower_lsp::lsp_types::{Position, Range};
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct LineIndex {
     /// Offset the beginning of each line, zero-based.
-    pub(crate) newlines: Vec<TextSize>,
+    pub newlines: Vec<TextSize>,
 }
 
 /// Line/Column information in native, utf8 format.
@@ -77,13 +79,33 @@ impl LineIndex {
     }
 }
 
-pub(crate) fn position(line_index: &LineIndex, offset: TextSize) -> Position {
+pub fn position(line_index: &LineIndex, offset: TextSize) -> Position {
     let line_col = line_index.line_col(offset);
     Position::new(line_col.line, line_col.col)
 }
 
-pub(crate) fn range(line_index: &LineIndex, range: TextRange) -> Range {
+pub fn range(line_index: &LineIndex, range: TextRange) -> Range {
     let start = position(line_index, range.start());
     let end = position(line_index, range.end());
     Range::new(start, end)
+}
+
+pub fn offset(line_index: &LineIndex, position: Position) -> Result<TextSize> {
+    let line_col = LineCol {
+        line: position.line,
+        col: position.character,
+    };
+    let text_size = line_index
+        .offset(line_col)
+        .ok_or_else(|| format_err!("Invalid offset"))?;
+    Ok(text_size)
+}
+
+pub fn text_range(line_index: &LineIndex, range: Range) -> Result<TextRange> {
+    let start = offset(line_index, range.start)?;
+    let end = offset(line_index, range.end)?;
+    match end < start {
+        true => Err(format_err!("Invalid Range").into()),
+        false => Ok(TextRange::new(start, end)),
+    }
 }
