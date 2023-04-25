e6fe9a963c7625e3b52de27fe5ee03e1aae0834d
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-04-25 20:30:17 +0800

    reparsing with Indel not working

--- crates/syntax/src/reparsing.rs
+++ crates/syntax/src/reparsing.rs
@@ -1,29 +1,37 @@
-use rowan::GreenNode;
+use rowan::{GreenNode, TextRange};
 use text_edit::Indel;
 
 use crate::{syntax_error::SyntaxError, syntax_node::SyntaxNode};
 
 pub fn reparse_card(root: &SyntaxNode, edit: &Indel) -> Option<(GreenNode, Vec<SyntaxError>)> {
-    use crate::syntax_node::SyntaxKind::*;
+    use crate::syntax_node::SyntaxKind::{self, *};
     let mut err = vec![];
     let green;
     let current_node = root.covering_element(edit.delete).as_node()?.clone();
-    match current_node.kind() {
-        GEOMETRY => {
+    let mom_is = |c: SyntaxKind| current_node.ancestors().find(|node| node.kind() == c);
+
+    if let Some(node) = mom_is(GEOMETRY) {
+        err.push(SyntaxError::new(
+            "don't edit geometry yet, naughty!".to_string(),
+            TextRange::default(),
+        ))
+    }
+
+    if let Some(node) = mom_is(CARD) {
+        err.push(SyntaxError::new(
+            "got a card!".to_string(),
+            TextRange::empty(node.text_range().start()),
+        ));
+    } else {
+        for i in current_node.ancestors() {
+            let duh = i.kind();
             err.push(SyntaxError::new(
-                "don't modify geometry in a text file yet! naughty!".to_string(),
-                current_node.text_range(),
+                format!("{:?}", duh),
+                TextRange::empty(current_node.text_range().start()),
             ));
-            green = root.green().into_owned();
-        }
-        _ => {
-            let Some(card) = current_node.ancestors().find(|node| node.kind() == CARD) else {return None};
-            err.push(SyntaxError::new(
-                "got a card!".to_string(),
-                card.text_range(),
-            ));
-            green = root.green().into_owned();
         }
     }
+    // green = root.green().into_owned();
+    green = root.green().into_owned();
     Some((green, err))
 }
