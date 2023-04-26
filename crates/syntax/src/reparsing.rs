1f346fa441c678f5c4634921a402e405b2d2b769
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-04-26 11:31:40 +0800

    reparse show tk as well

--- crates/syntax/src/reparsing.rs
+++ crates/syntax/src/reparsing.rs
@@ -6,15 +6,16 @@ use crate::{syntax_error::SyntaxError, syntax_node::SyntaxNode};
 pub fn reparse_card(root: &SyntaxNode, edit: &Indel) -> Option<(GreenNode, Vec<SyntaxError>)> {
     use crate::syntax_node::SyntaxKind::{self, *};
     let mut err = vec![];
-    let green;
     let range = edit.delete;
-    let current_node = root.covering_element(range).as_node().cloned();
-
+    let current = root.covering_element(range);
+    let current_node = current.as_node();
     let (message, range) = match current_node {
         Some(node) => (node.text().to_string(), node.text_range()),
-        None => ("edit too much at a time".to_string(), range),
+        None => {
+            let tk = current.into_token().unwrap();
+            (tk.text().to_string(), range)
+        }
     };
     err.push(SyntaxError::new(message, range));
-    green = root.green().into_owned();
-    Some((green, err))
+    Some((root.green().into_owned(), err))
 }
